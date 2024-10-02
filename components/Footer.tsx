8971ab337b952d2f364a76b2965a8314ca8fdcdb
Ravi Kumar <rravikumar27316@gmail.com>
2024-10-02 22:28:13 +0530

    push update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,24 +6,27 @@ export default function Footer() {
 	return (
 		<footer className=" border-t-[0.5px] py-10 flex items-center justify-center flex-col gap-5">
 			<div className="flex items-center gap-2">
-				<Link href="https://github.com/Chensokheng" target="_blank">
+				<Link href="https://github.com/r4ravikumar-dev" target="_blank">
 					<GitHubLogoIcon className="w-5 h-5 hover:scale-125 transition-all" />
 				</Link>
 				<Link
-					href="https://www.linkedin.com/in/chensokheng"
+					href="https://www.linkedin.com/in/r4ravikumar"
 					target="_blank"
 				>
 					<LinkedInLogoIcon className="w-5 h-5 hover:scale-125 transition-all" />
 				</Link>
 				<Link
-					href="https://www.youtube.com/c/DailyWebCoding"
+					href="https://www.instagram.com/_jha.ravi"
 					target="_blank"
 				>
 					<SiYoutube className="w-5 h-5 hover:scale-125 transition-all" />
 				</Link>
 			</div>
 			<h1 className="text-sm">
-				&copy; 2023 Chensokheng. All right reserved
+				This site is designed and developed by <a href="https://www.graphikx.in" className="underline">Graphikx India</a>
+			</h1>
+			<h1 className="text-sm -mt-4">
+				&copy; 2024 Graphikx. All right reserved
 			</h1>
 		</footer>
 	);
